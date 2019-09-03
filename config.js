ef8f92efe9c7845e022fff7886f5fd91eeed4e53
Nolan McGill <nomcgill@gmail.com>
2019-09-03 06:42:36 -0500

    fixing access

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://my-first-atlas-db-yegb9.mongodb.net/my-kitchen?retryWrites=true&w=majority'
-exports.PORT = process.env.PORT || 7000;
+exports.PORT = process.env.PORT || 8000;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
