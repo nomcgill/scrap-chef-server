5de4237c9221b2ae36562b6023942b01e3594168
Nolan McGill <nomcgill@gmail.com>
2019-09-04 18:51:04 -0500

    have server working!

--- config.js
+++ config.js
@@ -3,5 +3,3 @@
 
 exports.DATABASE_URL = 'mongodb+srv://clientuser99:poppsicles@my-first-atlas-db-yegb9.mongodb.net/my-kitchen?retryWrites=true&w=majority'
 exports.PORT = process.env.PORT || 8000;
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
