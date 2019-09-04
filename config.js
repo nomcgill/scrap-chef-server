2603c60f95619d6582e6cef30e62de7a32221822
Nolan McGill <nomcgill@gmail.com>
2019-09-04 17:37:45 -0500

    debuggin

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://clientuser99:poppsicles@my-first-atlas-db-yegb9.mongodb.net/my-kitchen?retryWrites=true&w=majority'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://my-first-atlas-db-yegb9.mongodb.net/my-kitchen?retryWrites=true&w=majority'
 exports.PORT = process.env.PORT || 8000;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
